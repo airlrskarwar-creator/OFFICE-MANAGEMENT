350884fe48156d65ad45b51caee39ddab075dd7f
airlrskarwar-creator <air.lrs.karwar@gmail.com>
2026-04-26 19:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ comm_sheet = spreadsheet.worksheet("CommFactDB")
 it_sheet = spreadsheet.worksheet("ITDB")
 dg_sheet = spreadsheet.worksheet("DGlog")
 hsd_sheet = spreadsheet.worksheet("HSDlog")
+electricity_sheet = spreadsheet.worksheet("Electricity")
 
 
 # =========================
@@ -102,13 +103,20 @@ def get_comm():
 def get_it():
     return jsonify(sheet_to_json(it_sheet))
 
+
 @app.route("/dg", methods=["GET"])
 def get_dg():
     return jsonify(sheet_to_json(dg_sheet))
 
+
 @app.route("/hsd", methods=["GET"])
 def get_hsd():
     return jsonify(sheet_to_json(hsd_sheet))
+
+
+@app.route("/electricity", methods=["GET"])
+def get_electricity():
+    return jsonify(sheet_to_json(electricity_sheet))
 
 
 
